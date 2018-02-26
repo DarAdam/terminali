22aee8769a2e29a3b6cac8145c502be2f07f133e
DarAdam <adamovicdarko@gmail.com>
2018-02-27 00:52:41 +0100

    deo testa za proveru lokacije

--- controler/unos_u_izmene_logovi.php
+++ controler/unos_u_izmene_logovi.php
@@ -3,11 +3,27 @@
 
 	foreach ($terminal as $serijski_broj) {
 		$tip_uređaja = 'terminal';
+		//proveri da li je uređaj na odgovarajućoj lokaciji:
+		$test = $conn->query('SELECT lokacija FROM uredjaji_lokacija WHERE tip_uredjaja = "terminal" AND serijski_broj = "' . $serijski_broj . '";');
+		$trenutna_lokacija = $test->fetch_assoc()['lokacija'];
+		echo 'trenutna lokacija terminala: ' . $trenutna_lokacija . '<br>';
+		if ($trenutna_lokacija == '') { $trenutna_lokacija = 'teren' };
+		if ($trenutna_lokacija != $polazna_lokacija) {}
+			
+		
+
+		//unesi podatak u bazu, u tabelu izmene_logovi
 		$sql = "INSERT INTO izmene_logovi (datum, operacija, polazna_lokacija, odredisna_lokacija, tip_uredjaja, serijski_broj, napomena) VALUES ('$datum', '$operacija', '$polazna_lokacija', '$odredisna_lokacija', '$tip_uređaja', '$serijski_broj', '$napomena')";
 		$conn->query($sql);
+		}
 	}
 	foreach ($qprox as $serijski_broj) {
-		$tip_uređaja = 'q prox';	
+		$tip_uređaja = 'q prox';
+		$test = $conn->query('SELECT lokacija FROM uredjaji_lokacija WHERE tip_uredjaja = "q prox" AND serijski_broj = "' . $serijski_broj . '";');
+		$trenutna_lokacija = $test->fetch_assoc()['lokacija'];
+		echo 'trenutna lokacija qprox-a: ' . $trenutna_lokacija . '<br>';
+
+
 		$sql = "INSERT INTO izmene_logovi (datum, operacija, polazna_lokacija, odredisna_lokacija, tip_uredjaja, serijski_broj, napomena) VALUES ('$datum', '$operacija', '$polazna_lokacija', '$odredisna_lokacija', '$tip_uređaja', '$serijski_broj', '$napomena')";
 		$conn->query($sql);
 	}
