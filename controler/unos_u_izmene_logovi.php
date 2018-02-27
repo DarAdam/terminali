bdc1016d52861cdd3cc4d003757d7fca41568768
DarAdam <adamovicdarko@gmail.com>
2018-02-27 01:11:11 +0100

    napravi posebnu petlju za test

--- controler/unos_u_izmene_logovi.php
+++ controler/unos_u_izmene_logovi.php
@@ -7,22 +7,28 @@
 		$test = $conn->query('SELECT lokacija FROM uredjaji_lokacija WHERE tip_uredjaja = "terminal" AND serijski_broj = "' . $serijski_broj . '";');
 		$trenutna_lokacija = $test->fetch_assoc()['lokacija'];
 		echo 'trenutna lokacija terminala: ' . $trenutna_lokacija . '<br>';
-		if ($trenutna_lokacija == '') { $trenutna_lokacija = 'teren' };
-		if ($trenutna_lokacija != $polazna_lokacija) {}
+		if ($trenutna_lokacija != $polazna_lokacija && $polazna_lokacija != 'teren') {
+			echo '<br>Nije dobar unos za terminal<br>';
+			die;
+		}
+
 			
 		
 
 		//unesi podatak u bazu, u tabelu izmene_logovi
 		$sql = "INSERT INTO izmene_logovi (datum, operacija, polazna_lokacija, odredisna_lokacija, tip_uredjaja, serijski_broj, napomena) VALUES ('$datum', '$operacija', '$polazna_lokacija', '$odredisna_lokacija', '$tip_uređaja', '$serijski_broj', '$napomena')";
 		$conn->query($sql);
-		}
+		
 	}
 	foreach ($qprox as $serijski_broj) {
 		$tip_uređaja = 'q prox';
 		$test = $conn->query('SELECT lokacija FROM uredjaji_lokacija WHERE tip_uredjaja = "q prox" AND serijski_broj = "' . $serijski_broj . '";');
 		$trenutna_lokacija = $test->fetch_assoc()['lokacija'];
 		echo 'trenutna lokacija qprox-a: ' . $trenutna_lokacija . '<br>';
-
+		if ($trenutna_lokacija != $polazna_lokacija && $polazna_lokacija != 'teren') {
+			echo '<br>Nije dobar unos za Qprox<br>';
+			die;
+		}
 
 		$sql = "INSERT INTO izmene_logovi (datum, operacija, polazna_lokacija, odredisna_lokacija, tip_uredjaja, serijski_broj, napomena) VALUES ('$datum', '$operacija', '$polazna_lokacija', '$odredisna_lokacija', '$tip_uređaja', '$serijski_broj', '$napomena')";
 		$conn->query($sql);
