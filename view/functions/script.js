ed02d613ebae8d6f590a3e35375ad406129e7222
DarAdam <adamovicdarko@gmail.com>
2018-03-07 17:37:53 +0100

    sređeno dodavanje novog reda

--- view/functions/script.js
+++ view/functions/script.js
@@ -3,9 +3,24 @@
 	};
 
 function dodaj_red() {
-	var line = document.createElement("tr");
-	var text = document.createTextNode("<td>broj</td>");
-	line.appendChild(text);
-	//document.getElementById("tabela").appendChild(line);
-	document.getElementById("dodajdugme").insertBefore(line);
+	var table = document.getElementById("tabela");
+	var ukred = table.getElementsByTagName("tr").length;
+	var red = table.insertRow(ukred-1);
+	var cell1 = red.insertCell(0);
+	var cell2 = red.insertCell(1);
+	var cell3 = red.insertCell(2);
+
+	var inputT = document.createElement('input');
+	var att = document.createAttribute("name");
+	att.value = "terminal[]";
+	inputT.setAttributeNode(att);
+
+	var inputQ = document.createElement('input');
+	var attQ = document.createAttribute("name");
+	attQ.value = "qprox[]";
+	inputQ.setAttributeNode(attQ);
+
+	cell1.innerHTML = ukred - 1;
+	cell2.appendChild(inputT);
+	cell3.appendChild(inputQ);
 }
